5786caa28afcc3ee0d37a732db8416cb556aae08
MdhaffarMariam <mariammdhaffar.94@gmail.com>
2022-09-22 12:25:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe width="1000" height="1300" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
+  );
+}
+
+export default App;
